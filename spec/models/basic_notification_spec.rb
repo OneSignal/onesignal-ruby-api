0d3ca10cb039b3eccc940a707d5e8ba8f776e876
OneSignal <noreply@onesignal.com>
2026-07-28 21:34:26 +0000

    feat: add v5.11.0 package updates

--- spec/models/basic_notification_spec.rb
+++ spec/models/basic_notification_spec.rb
@@ -303,6 +303,12 @@ describe OneSignal::BasicNotification do
     end
   end
 
+  describe 'test attribute "global_image"' do
+    it 'should work' do
+      # assertion here. ref: https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers
+    end
+  end
+
   describe 'test attribute "huawei_big_picture"' do
     it 'should work' do
       # assertion here. ref: https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers
